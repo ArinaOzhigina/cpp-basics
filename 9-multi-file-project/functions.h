aaff0378bfe9cffd172c0a1ace80c98bab12a0e6
Arina <43355948+ArinaOzhigina@users.noreply.github.com>
2018-11-19 07:49:36 +0300

    added files

---
+++ 9-multi-file-project/functions.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef Functions
+#define Functions
+
+double negSum(double *myarray, int count);
+double * sortArray(double *myarray, int count);
+double getMultiple(double *myarray, int count);
+
+#endif 
